367e954d9951b057c39047df20fe58c4e1f76da3
Mehul Swaroop <mehulswaroop19@gmail.com>
2017-05-18 16:37:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.red').click(function() {
+        $('.red').fadeOut('slow')
+    });
+    $('.green').hover(function() {
+        $('.green').fadeOut()
+    });
+})
